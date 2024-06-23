6f340aab2a63c103e15baaf9b7f4eba782a5cad2
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-06-23 15:13:44 +0300

    test

--- app/(routes)/(site)/page.tsx
+++ app/(routes)/(site)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="bg-slate-600">
       <Button variant="destructive" size="icon">deneme</Button>
 
-      <ModeToggle/>
+      <ModeToggle/> 
 
     </div>
   );
