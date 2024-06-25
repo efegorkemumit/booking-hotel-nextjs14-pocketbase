e097eb72521ac78c3bfdf67c0e503f5515a8b847
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-06-25 14:03:25 +0300

    Login Register Router and Menu

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,11 +1,63 @@
+'use client'
+
+import { Loader2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 interface AuthLayoutProps {
   children: React.ReactNode
 }
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  const [isUserLoaded, setIsUserLoaded] = useState(false);
+
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+
+        const authData = localStorage.getItem("pocketbase_auth");
+        if (authData) {
+          const { token, model } = JSON.parse(authData);
+          setUser(model)
+        }
+        
+      } catch (error) {
+        
+      }
+      finally{
+        setIsUserLoaded(true)
+      }
+       
+    }
+    fetchUser();
+  }, [])
+
+  useEffect(()=>{
+    if(isUserLoaded && user && user.email){
+      router.push("/")
+    }
+  },[isUserLoaded, user, router])
+
+  if(!isUserLoaded){
+
+    return(
+      <div className='flex items-center justify-center h-screen'>
+        <Loader2 size={48} className='animate-spin'/>
+
+      </div>
+    )
+  }
+
+  if(user && user.email){
+    return null;
+  }
+
+
   return (
     <div className='h-screen flex justify-center items-center bgone'>
       <div className='hidden lg:block w-1/2 h-full'>
--- app/(routes)/_components/UserToogle.tsx
+++ app/(routes)/_components/UserToogle.tsx
@@ -1,12 +1,60 @@
+'use client'
+
 import { UserIcon } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 const UserToogle = () => {
+
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const authData = localStorage.getItem("pocketbase_auth");
+      if (authData) {
+        const { token, model } = JSON.parse(authData);
+        setUser(model)
+      }
+    }
+    fetchUser();
+  }, [])
+
+
   return (
-    <Link href="/auth/login">
-    <UserIcon className='text-white dark:text-white'/>
-    </Link>
+    <div>
+      {user ? (
+        <DropdownMenu>
+        <DropdownMenuTrigger className='flex text-white text-xs justify-center items-center'> 
+          <UserIcon className='text-white dark:text-white' /> {user.username}
+          </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem  onClick={()=>{localStorage.removeItem('pocketbase_auth'); setUser(null)}}>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      )
+        :
+        (
+          <Link href="/auth/login">
+            <UserIcon className='text-white dark:text-white' />
+          </Link>
+        )
+
+      }
+    </div>
   )
 }
 
